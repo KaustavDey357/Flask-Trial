f331c522e5d6886c1b9fcc8f2b7d5c62387c25b0
deykaustav357 <deykaustav357@gmail.com>
2025-06-26 17:39:12 +0530

    configured table and db

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template , url_for
+from flask import Flask , render_template , url_for , request , redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -6,15 +6,33 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
-class todo(db.Model):
+class Todo(db.Model):
     id = db.Column(db.Integer , primary_key=True)
-    task = db.Column(db.String(200), nullable=False)
+    Task = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Integer , default =0)
-    date_created = db.Column(db.DateTime , default=datetime.utcnow)
+    date_created = db.Column(db.DateTime , default=datetime.utcnow())
+    
+    def _repr_ (self):
+        return '<Task %r>' % self.id 
 
 
-@app.route('/')
+@app.route('/' , methods=['GET','POST'])
 def func():
-    return render_template('index.html')
+    if(request.method == 'GET'):
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html' , tasks=tasks)
+    else : 
+        new_content = request.form['content']
+        new_task = Todo(Task=new_content)
+        
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Error!"
+        
+    
+        
 if __name__ == "__main__":
     app.run( debug = True)
